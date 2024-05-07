cd169691cd5b22fbc1636d2e7abe72030ef7c8ad
Rezwan <168479900+Rezwan-react@users.noreply.github.com>
2024-05-07 19:31:55 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let age = prompt("tomr boyos koto") 
+
+    if(age>20){
+
+        let income = prompt('tomr income koto')
+
+            if(income >=30000){
+                alert('biye koro')
+            }
+            else{
+                alert('kete porp')
+            }
+        
+    }
+
+else{
+    alert('boro hou')
+}
